ed6ff00ee13ba6a110bb8415c45b4c6334d2aaa0
Derek Wickern <dwickern@gmail.com>
2017-07-03 08:05:12 -0700

    Add failed test for json-with-defaults stackoverflow

--- play-json/shared/src/test/scala/play/api/libs/json/MacroSpec.scala
+++ play-json/shared/src/test/scala/play/api/libs/json/MacroSpec.scala
@@ -342,6 +342,12 @@ class MacroSpec extends WordSpec with MustMatchers
       )
     }
 
+    "handle case class with default values, format defined in companion object" in {
+      val json = Json.obj("id" -> 15)
+      val expected = WithDefaultInCompanion(15, "a")
+      Json.fromJson[WithDefaultInCompanion](json).get mustEqual expected
+    }
+
     "handle case class with default values inner optional case class containing default values" when {
       implicit val cfg = JsonConfiguration(JsonNaming.Identity)
       implicit val withDefaultFormat = Json.using[Json.MacroOptions with Json.DefaultValues].format[WithDefault]
@@ -672,4 +678,9 @@ class MacroSpec extends WordSpec with MustMatchers
 
   type OptionalInt = Option[Int]
   case class UsingAlias(v: OptionalInt)
+
+  case class WithDefaultInCompanion(id: Int, a: String = "a")
+  object WithDefaultInCompanion {
+    implicit val format: OFormat[WithDefaultInCompanion] = Json.using[Json.WithDefaultValues].format[WithDefaultInCompanion]
+  }
 }
