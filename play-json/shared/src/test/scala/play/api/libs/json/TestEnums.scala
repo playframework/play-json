65752628b5ee2536d5b68359c62468933836dbbb
abrasha <aabrasha@gmail.com>
2018-05-03 13:21:14 +0300

    - extract test enums to separate object - split testing into reads and writes specs

---
+++ play-json/shared/src/test/scala/play/api/libs/json/TestEnums.scala
@@ -0,0 +1,21 @@
+package play.api.libs.json
+
+object TestEnums {
+
+  object EnumWithCustomNames extends Enumeration {
+    type EnumWithCustomNames = Value
+
+    val (customEnum1, customEnum2) = (Value("ENUM1"), Value("ENUM2"))
+
+    implicit val format: Format[EnumWithCustomNames] = Json.formatEnum(this)
+  }
+
+  object EnumWithDefaultNames extends Enumeration {
+    type EnumWithDefaultNames = Value
+
+    val defaultEnum1, defaultEnum2 = Value
+
+    implicit val format: Format[EnumWithDefaultNames] = Json.formatEnum(this)
+  }
+
+}
