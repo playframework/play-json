75669aa6e712579ff17f3e3345be0df1760d78c5
s.hardt <s.hardt@micromata.de>
2018-08-31 18:23:38 +0200

    Test fix

--- play-json/shared/src/main/scala/play/api/libs/json/JavaEnumFormat.scala
+++ play-json/shared/src/main/scala/play/api/libs/json/JavaEnumFormat.scala
@@ -1,20 +1,20 @@
 package play.api.libs.json
 
 /**
-  * Helper for format a java enum into or from json
-  *
-  * public enum TestEnum {
-  *
-  *   TEST1,
-  *   TEST2,
-  *   OKAY
-  *
-  * }
-  *
-  * implicit val  TestEnumFormat = JavaEnumFormat.format[TestEnum]
-  *
-  * @author Sebastian Hardt (s.hardt@micromata.de)
-  */
+ * Helper for format a java enum into or from json
+ *
+ * public enum TestEnum {
+ *
+ *   TEST1,
+ *   TEST2,
+ *   OKAY
+ *
+ * }
+ *
+ * implicit val  TestEnumFormat = JavaEnumFormat.format[TestEnum]
+ *
+ * @author Sebastian Hardt (s.hardt@micromata.de)
+ */
 object JavaEnumFormat {
   def format[A <: Enum[A]](implicit m: scala.reflect.Manifest[A]): Format[A] = {
     new JsonJavaEnumFormat[A]
@@ -22,11 +22,11 @@ object JavaEnumFormat {
 }
 
 /**
-  * Format for java enums from and to json
-  *
-  * @param classType the classType of the enum
-  * @tparam A the type of the enum
-  */
+ * Format for java enums from and to json
+ *
+ * @param classType the classType of the enum
+ * @tparam A the type of the enum
+ */
 class JsonJavaEnumFormat[A <: Enum[A]](implicit m: scala.reflect.Manifest[A]) extends Format[A] {
   def reads(json: JsValue) = JsSuccess(Enum.valueOf[A](m.runtimeClass.asInstanceOf[Class[A]], json.as[String]))
 
--- play-json/shared/src/test/scala/play/api/libs/json/JavaTestsEnumsFormat.scala
+++ play-json/shared/src/test/scala/play/api/libs/json/JavaTestsEnumsFormat.scala
@@ -1,8 +1,6 @@
 package play.api.libs.json
 
-
 object JavaTestsEnumsFormat {
-  implicit val testsEnumsFormat = JavaEnumFormat[JavaTestEnum]
+  implicit val testsEnumsFormat = JavaEnumFormat.format[JavaTestEnum]
 }
-
 
