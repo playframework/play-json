fa24e7d5f3119a01ed6de90553a835e2461263ed
Dale Wijnand <dale.wijnand@gmail.com>
2018-11-14 06:10:31 +0000

    Make PlayJsonModule final

--- play-json/jvm/src/main/scala/play/api/libs/json/jackson/JacksonJson.scala
+++ play-json/jvm/src/main/scala/play/api/libs/json/jackson/JacksonJson.scala
@@ -34,7 +34,7 @@ import scala.collection.mutable.{ ArrayBuffer, ListBuffer }
  *   val jsValue = mapper.readValue("""{"foo":"bar"}""", classOf[JsValue])
  * }}}
  */
-class PlayJsonModule(parserSettings: JsonParserSettings) extends SimpleModule("PlayJson", Version.unknownVersion()) {
+final class PlayJsonModule(parserSettings: JsonParserSettings) extends SimpleModule("PlayJson", Version.unknownVersion()) {
   override def setupModule(context: SetupContext): Unit = {
     context.addDeserializers(new PlayDeserializers(parserSettings))
     context.addSerializers(new PlaySerializers(parserSettings))
