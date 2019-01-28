7c5936fd93cc5dadee2fc112d05593067e15c510
João Ferreira <jtjeferreira@gmail.com>
2019-01-28 23:19:40 +0000

    Adds support for MapView (#231)
    * test first
    
    * fixes #225
    
    * Unnest the type to improve readability

---
+++ play-json/shared/src/main/scala-2.13+/play/api/libs/json/MapWrites.scala
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2009-2019 Lightbend Inc. <https://www.lightbend.com>
+ */
+
+package play.api.libs.json
+
+import scala.collection.MapOps
+import scala.collection.IterableOps
+
+object MapWrites {
+
+  type Map[K, V] = MapOps[K, V, CC, _]
+
+  //see scala.collection.AnyConstr
+  private[json] type AnyConstr[X] = Any
+
+  // see scala.collections.MapOps
+  private[json] type CC[X, +Y] = IterableOps[_, AnyConstr, _]
+
+  /**
+   * Serializer for Map[String,V] types.
+   */
+  def mapWrites[V: Writes]: OWrites[Map[String, V]] = {
+    val w = implicitly[Writes[V]]
+
+    OWrites[Map[String, V]] { ts => JsObject(ts.mapValues(w.writes(_)).toSeq) }
+  }
+}
