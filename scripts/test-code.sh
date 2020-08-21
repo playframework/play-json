4489134f6e35e7702ba5c9caf225b914a4ff2907
Enno Runne <458526+ennru@users.noreply.github.com>
2020-08-21 16:06:39 +0200

    Echo the Scala version

--- scripts/test-code.sh
+++ scripts/test-code.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
+echo TRAVIS_SCALA_VERSION=$TRAVIS_SCALA_VERSION
 sbt -DscalaJSStage=full ++$TRAVIS_SCALA_VERSION test publishLocal || exit 1
 sbt ++$TRAVIS_SCALA_VERSION docs/test || exit 2
