7dcef4f9c4ef7770f6b57fabe2757ae24caf2fb9
Enno Runne <458526+ennru@users.noreply.github.com>
2020-08-21 16:09:36 +0200

    Rename the environment value

--- scripts/test-code.sh
+++ scripts/test-code.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-echo TRAVIS_SCALA_VERSION=$TRAVIS_SCALA_VERSION
-sbt -DscalaJSStage=full ++$TRAVIS_SCALA_VERSION test publishLocal || exit 1
-sbt ++$TRAVIS_SCALA_VERSION docs/test || exit 2
+echo SCALA_VERSION=$SCALA_VERSION
+sbt -DscalaJSStage=full ++$SCALA_VERSION test publishLocal || exit 1
+sbt ++$SCALA_VERSION docs/test || exit 2
