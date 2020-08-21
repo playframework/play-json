a77d5c106c888e7be21223af5bd4729b70188c92
Enno <458526+ennru@users.noreply.github.com>
2020-08-21 16:27:27 +0200

    Make Travis happy (#504)

--- scripts/test-code.sh
+++ scripts/test-code.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
-sbt -DscalaJSStage=full ++$TRAVIS_SCALA_VERSION test publishLocal || exit 1
-sbt ++$TRAVIS_SCALA_VERSION docs/test || exit 2
+echo SCALA_VERSION=$SCALA_VERSION
+sbt -DscalaJSStage=full ++$SCALA_VERSION test publishLocal || exit 1
+sbt ++$SCALA_VERSION docs/test || exit 2
