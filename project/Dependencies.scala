c2023b19bfeb6bdd5010dfddb2a088c13a52012a
Scala Steward <me@scala-steward.org>
2021-11-04 11:05:01 +0100

    scala-compiler, scala-library, ... 2.13.7 (was 2.13.6)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,6 +1,6 @@
 object Dependencies {
   // scalaVersion needs to be kept in sync with travis-ci
   val Scala212 = "2.12.15"
-  val Scala213 = "2.13.6"
+  val Scala213 = "2.13.7"
   val Scala3   = "3.0.2"
 }
