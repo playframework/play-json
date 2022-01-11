15f4bc0d5feef2c099667ff21f2353fbfcd67486
Matthias Kurz <m.kurz@irregular.at>
2022-01-11 12:26:49 +0100

    Scala 2.13.8

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,6 +3,6 @@ object Dependencies {
   //   - GA (.github/workflows/build-test.yml)
   //   - Mergify conditions (.mergify.yml)
   val Scala212 = "2.12.15"
-  val Scala213 = "2.13.7"
+  val Scala213 = "2.13.8"
   val Scala3   = "3.0.2"
 }
