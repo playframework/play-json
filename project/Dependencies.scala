59a5fd5b75e8d07ae57298723ec175939f429089
Matthias Kurz <m.kurz@irregular.at>
2022-05-19 13:30:11 +0200

    Scala 3.1.3-RC3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,5 +2,5 @@ object Dependencies {
   // Sync with GA (.github/workflows/build-test.yml)
   val Scala212 = "2.12.15"   // sync! see comment above
   val Scala213 = "2.13.8"    // sync! see comment above
-  val Scala3   = "3.1.3-RC2" // sync! see comment above
+  val Scala3   = "3.1.3-RC3" // sync! see comment above
 }
