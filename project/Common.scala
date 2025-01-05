e24c1ca922ef9bc32f9370aa9e013fe92c4f1371
PJ Fanning <pjfanning@users.noreply.github.com>
2025-01-05 19:23:02 +0100

    change pom

--- project/Common.scala
+++ project/Common.scala
@@ -11,19 +11,27 @@ object Common extends AutoPlugin {
 
   override def globalSettings =
     Seq(
-      organization         := "org.playframework",
+      organization         := "com.github.pjfanning",
       organizationName     := "The Play Framework Project",
-      organizationHomepage := Some(url("https://playframework.com/")),
-      homepage             := Some(url(s"https://github.com/playframework/${repoName}")),
+      organizationHomepage := Some(url("https://github.com/pjfanning")),
+      homepage             := Some(url(s"https://github.com/pjfanning/${repoName}")),
       licenses             := Seq("Apache-2.0" -> url("https://www.apache.org/licenses/LICENSE-2.0.html")),
       scalacOptions ++= Seq("-deprecation", "-feature", "-unchecked", "-encoding", "utf8"),
       javacOptions ++= Seq("-encoding", "UTF-8", "-Xlint:-options"),
-      developers += Developer(
-        "playframework",
-        "The Play Framework Contributors",
-        "contact@playframework.com",
-        url("https://github.com/playframework")
+      developers ++= Seq(
+        Developer(
+          "playframework",
+          "The Play Framework Contributors",
+          "contact@playframework.com",
+          url("https://github.com/playframework")
+        ),
+        Developer(
+          "pjfanning",
+          "PJ Fanning",
+          "",
+          url("https://github.com/pjfanning")
+        )
       ),
-      description := "Play JSON"
+      description := "Play JSON (fork)"
     )
 }
