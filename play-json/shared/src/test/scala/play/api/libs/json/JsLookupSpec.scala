f7630490b86c7f75e0e92bf53ec9d5f611da60b0
Dan Kotovski <dan.kotovski@granulate.io>
2025-01-16 14:57:44 +0200

    Added test for JsUndefined.asOpt function

---
+++ play-json/shared/src/test/scala/play/api/libs/json/JsLookupSpec.scala
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) from 2022 The Play Framework Contributors <https://github.com/playframework>, 2011-2021 Lightbend Inc. <https://www.lightbend.com>
+ */
+
+package play.api.libs.json
+
+import play.api.libs.json.Json._
+
+import org.scalatest.matchers.must.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+class JsLookupSpec extends AnyWordSpec with Matchers {
+  "JsLookupResult" should {
+    val obj = Json.obj(
+      "field" -> 123
+    )
+    val result = obj \ "missingField"
+
+    "return JsUndefined when a key is missing" in {
+      result mustBe a[JsUndefined]
+    }
+
+    "return None when calling asOpt on JsUndefined" in {
+      result.asOpt[Int] mustEqual None
+    }
+  }
+}
