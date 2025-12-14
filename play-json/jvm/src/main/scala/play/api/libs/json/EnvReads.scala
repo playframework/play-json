9eb72888650230c6c61401d29675cb650e91d315
Gaël Jourdan-Weil <gjourdanweil@gmail.com>
2025-12-14 18:04:50 +0100

    🧹 chore: upgrade to Jackson 3

--- play-json/jvm/src/main/scala/play/api/libs/json/EnvReads.scala
+++ play-json/jvm/src/main/scala/play/api/libs/json/EnvReads.scala
@@ -26,9 +26,9 @@ import java.util.Locale
 
 import scala.util.control.NonFatal
 
-import com.fasterxml.jackson.databind.JsonNode
-import com.fasterxml.jackson.databind.node.ArrayNode
-import com.fasterxml.jackson.databind.node.ObjectNode
+import tools.jackson.databind.JsonNode
+import tools.jackson.databind.node.ArrayNode
+import tools.jackson.databind.node.ObjectNode
 
 import play.api.libs.json.jackson.JacksonJson
 
